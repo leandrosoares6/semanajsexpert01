3d11a1d97b7a5b093230f3dfd7bc5c18f14af0ef
Leandro Neves <leandrosoares6@hotmail.com>
2020-10-30 16:31:11 -0300

    Deploy no CloudFront da AWS com Serveless Framework

--- public/bandersnatch/src/network.js
+++ public/bandersnatch/src/network.js
@@ -12,4 +12,35 @@ class Network {
     const response = await fetch(url);
     return response.arrayBuffer();
   }
+
+  async getProperResolution(url) {
+    const startMs = Date.now();
+    const response = await fetch(url);
+    await response.arrayBuffer();
+    const endMs = Date.now();
+    const durationInMs = (endMs - startMs);
+
+    console.log('durationInMs', durationInMs);
+
+    // calculate duration instead of throughPut
+    const resolutions = [
+      // 20 seconts - worst case
+      { start: 3001, end: 20000, resolution: 144 },
+      // less than or equal to 3 seconds
+      { start: 901, end: 3000, resolution: 360 },
+      // less than 1 second
+      { start: 0, end: 900, resolution: 720 },
+    ];
+
+    const item = resolutions.find(item => {
+      return item.start <= durationInMs && item.end >= durationInMs;
+    });
+
+    const LOWEST_RESOLUTION = 144;
+
+    // greater than 30 seconds
+    if (!item) return LOWEST_RESOLUTION;
+
+    return item.resolution;
+  }
 }
--- public/bandersnatch/src/videoPlayer.js
+++ public/bandersnatch/src/videoPlayer.js
@@ -1,10 +1,14 @@
 class VideoMediaPlayer {
-  constructor({ manifestJSON, network }) {
+  constructor({ manifestJSON, network, videoComponent }) {
     this.manifestJSON = manifestJSON;
     this.network = network;
+    this.videoComponent = videoComponent;
+
     this.videoElement = null;
     this.sourceBuffer = null;
+    this.activeItem = {};
     this.selected = {};
+    this.selections = [];
     this.videoDuration = 0;
   }
 
@@ -44,13 +48,66 @@ class VideoMediaPlayer {
       // prevent running as "LIVE"
       mediaSource.duration = this.videoDuration;
       await this.fileDownload(selected.url);
+      setInterval(this.waitForQuestions.bind(this), 200);
     };
   }
 
+  waitForQuestions() {
+    const currentTime = parseInt(this.videoElement.currentTime);
+    const option = this.selected.at === currentTime;
+
+    if (!option) return;
+
+    // prevent open the modal 2 times in same second
+    if (this.activeItem.url === this.selected.url) return;
+    this.videoComponent.configureModal(this.selected.options);
+    this.activeItem = this.selected;
+  }
+
+  async currentFileResolution() {
+    const LOWEST_RESOLUTION = 144;
+    const prepareUrl = {
+      url: this.manifestJSON.finalizar.url,
+      fileResolution: LOWEST_RESOLUTION,
+      fileResolutionTag: this.manifestJSON.fileResolutionTag,
+      hostTag: this.manifestJSON.hostTag,
+    }
+
+    const url = this.network.parseManifestURL(prepareUrl);
+    return this.network.getProperResolution(url);
+  }
+
+  async nextChunk(data) {
+    const key = data.toLowerCase();
+    const selected = this.manifestJSON[key];
+
+    this.selected = {
+      ...selected,
+      // adjust the time when show modal based in current time
+      at: parseInt(this.videoElement.currentTime + selected.at),
+    };
+    this.manageLag(this.selected);
+
+    this.videoElement.play();
+    // let the rest of the video run while downloading the new video
+    await this.fileDownload(selected.url);
+  }
+
+  manageLag(selected) {
+    if (!!~this.selections.indexOf(selected.url)) {
+      selected.at += 5;
+      return;
+    }
+
+    this.selections.push(selected.url);
+  }
+
   async fileDownload(url) {
+    const fileResolution = await this.currentFileResolution();
+    console.log('currentResolution', fileResolution);
     const prepareUrl = {
       url,
-      fileResolution: 360,
+      fileResolution,
       fileResolutionTag: this.manifestJSON.fileResolutionTag,
       hostTag: this.manifestJSON.hostTag,
     };
@@ -59,12 +116,12 @@ class VideoMediaPlayer {
     this.setVideoPlayerDuration(finalUrl);
     const data = await this.network.fetchFile(finalUrl);
 
-    return this.processBufferSegments(data);  
+    return this.processBufferSegments(data);
   }
 
   setVideoPlayerDuration(finalURL) {
     const bars = finalURL.split('/');
-    const [name, videoDuration] = bars[bars.length - 1].split('-');
+    const [, videoDuration] = bars[bars.length - 1].split('-');
     this.videoDuration += parseFloat(videoDuration);
   }
 
